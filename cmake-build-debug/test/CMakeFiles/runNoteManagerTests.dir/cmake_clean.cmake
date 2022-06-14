479d5b76ce1c7ecebb34fd2922a2ed48ecb79f5a
caterinagiardi <caterina.giardi@stud.unifi.t>
2022-06-15 00:21:53 +0200

    1

---
+++ cmake-build-debug/test/CMakeFiles/runNoteManagerTests.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runNoteManagerTests.dir/CollectionTest.cpp.o"
+  "CMakeFiles/runNoteManagerTests.dir/MenuTest.cpp.o"
+  "CMakeFiles/runNoteManagerTests.dir/NoteManagerFixture.cpp.o"
+  "CMakeFiles/runNoteManagerTests.dir/NoteTest.cpp.o"
+  "CMakeFiles/runNoteManagerTests.dir/runAllTests.cpp.o"
+  "runNoteManagerTests"
+  "runNoteManagerTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runNoteManagerTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
